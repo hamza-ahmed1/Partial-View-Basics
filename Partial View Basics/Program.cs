908c17ab35318dc6f504629f1c9b9732a00c66a6
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-08-10 01:08:19 -0700

    Initial commit

---
+++ Partial View Basics/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+#region Summary
+/*
+ * Usage:if we want common markup in some of the files from all files then we will use partial view but and if we want to use common markup across all view then for this we have
+ * learned concept of master page:
+ * 
+ */
+#endregion
